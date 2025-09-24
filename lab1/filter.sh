6531739a0646a066588faf04d07b8fb531a090d3
belen-aedo <belen.aedo@usach.cl>
2025-09-24 20:08:26 -0300

    actualizatr

--- lab1/filter.sh
+++ lab1/filter.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#
-#Entradas: datos por stdin, -c cpu, -m memoria, -r expresion regular
-#Salidas: datos filtrados
-#Descripción: filtra procesos segun los criterios dados
+
+#entradas:  -c cpu, -m memoria, -r expresion regular
+#salidas: datos filtrados
+#descripcion: filtraremos procesos segun los criterios dados
 
 #variables
 cpu_min=""
 mem_min=""
 expre=""
 
-# Leer argumentos
+#leemos los argumentos dados por el usuario
 while [ $# -gt 0 ]; do
     case $1 in
         -c) cpu_min=$2; shift 2 ;; #eliminamos 2 elementos con el shift
@@ -20,15 +20,15 @@ while [ $# -gt 0 ]; do
     esac
 done
 
-#funcion la cual sirve para verificar si un numero es mayor o igual
-#Entradas: $1 = numero1, $2 = numero2
-#Salidas: 0 si num1 >= num2, 1 si no
-#Descripcion: compara dos numeros decimales
-es_mayor_igual() {
+#funcion la cual sirve para verificar si un numero es mayor o igual, y vemos si cumple con las condiciones
+#entradas: $1 = numero1, $2 = numero2
+#salidas: 0 si num1 >= num2, 1 si no
+#descripcion: compara dos numeros decimales
+comparacion() {
     local num1="$1"
     local num2="$2"
     
-    # Usar awk para comparar decimales
+    #usamos el  awk para comparar decimales
     resultado=$(echo "$num1 $num2" | awk '{if ($1 >= $2) print "si"; else print "no"}')
     
     if [ "$resultado" = "si" ]; then
@@ -40,13 +40,13 @@ es_mayor_igual() {
 
 #recorremos las lineas dadas
 while read linea; do
-    #se salta las lineas vacias
+    #se salta las lineas que se encuentran vacias
     if [ -z "$linea" ]; then
         continue
     fi
     
     #sacamos los elememtos y los asignamos a variables
-    timestamp=$(echo "$linea" | cut -f1)
+    tiempo=$(echo "$linea" | cut -f1)
     pid=$(echo "$linea" | cut -f2)
     uid=$(echo "$linea" | cut -f3)
     comm=$(echo "$linea" | cut -f4)
@@ -58,14 +58,14 @@ while read linea; do
     
     #cpu minima
     if [ -n "$cpu_min" ]; then
-        if ! es_mayor_igual "$pcpu" "$cpu_min"; then
+        if ! comparacion "$pcpu" "$cpu_min"; then
             pasa_filtros=false
         fi
     fi
     
     #memoria minima
     if [ -n "$mem_min" ]; then
-        if ! es_mayor_igual "$pmem" "$mem_min"; then
+        if ! comparacion "$pmem" "$mem_min"; then
             pasa_filtros=false
         fi
     fi
